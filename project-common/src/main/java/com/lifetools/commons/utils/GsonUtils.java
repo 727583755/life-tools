4ead9f1fec98a87a19103eefa8037e9f2c603182
zhengke <ke.zheng@quncaotech.com>
2016-07-04 12:00:12 +0800

    优化

--- project-common/src/main/java/com/lifetools/commons/utils/GsonUtils.java
+++ project-common/src/main/java/com/lifetools/commons/utils/GsonUtils.java
@@ -5,7 +5,7 @@ import com.google.gson.GsonBuilder;
 
 /**
  * 处理json字符串与对象之间关系的工具类
- * @author zk
+ * @author Zheng.Ke
  * @date 2015年8月27日 下午4:53:45
  */
 public class GsonUtils {
@@ -25,7 +25,15 @@ public class GsonUtils {
 	public static String toJson(Object obj, String datePattern, Boolean isPrettyPrinting) {
 		return toJson(obj, datePattern, isPrettyPrinting, null);
 	}
-	
+
+	/**
+	 * 对象转成json字符串
+	 * @param obj  要转的对象
+	 * @param datePattern   日期格式
+	 * @param isPrettyPrinting  是否格式化json字符串
+	 * @param isExportNull   是否输出null值
+	 * @return
+	 */
 	public static String toJson(Object obj, String datePattern, Boolean isPrettyPrinting, Boolean isExportNull) {
 		GsonBuilder builder = new GsonBuilder();
 		
@@ -43,7 +51,14 @@ public class GsonUtils {
 		Gson gson = builder.create();
 		return gson.toJson(obj);
 	}
-	
+
+	/**
+	 * json字符串转化为对象
+	 * @param jsonStr
+	 * @param clazz
+	 * @param <T>
+	 * @return
+	 */
 	public static <T> T jsonStrToEntity(String jsonStr, Class<T> clazz) {
 		Gson gson = new GsonBuilder().create();
 		return gson.fromJson(jsonStr, clazz);
