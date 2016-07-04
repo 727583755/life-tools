13017f4d08951c1181ff419def4c97066b97f1d0
zhengke <ke.zheng@quncaotech.com>
2016-07-04 11:48:40 +0800

    优化

--- project-common/src/main/java/com/lifetools/commons/utils/EncryptUtils.java
+++ project-common/src/main/java/com/lifetools/commons/utils/EncryptUtils.java
@@ -7,7 +7,7 @@ import org.apache.commons.codec.binary.Base64;
 
 /**
  * 加密解密工具类
- * @author zk
+ * @author Zheng.Ke
  * @date 2016年2月23日 下午3:13:03
  */
 public class EncryptUtils {
@@ -25,18 +25,40 @@ public class EncryptUtils {
 		}
 	}
 
+	/**
+	 * MD5加密
+	 * @param origin
+	 * @return
+	 */
 	public static String hashByMD5(String origin) {
 		return hash("MD5", origin);
 	}
 
+	/**
+	 * SHA1加密
+	 * @param origin
+	 * @return
+	 */
 	public static String hashBySHA1(String origin) {
 		return hash("SHA-1", origin);
 	}
 
+	/**
+	 * BASE64加密
+	 * @param origin  明文
+	 * @param charset 字符编码
+	 * @return
+	 */
 	public static String encodeByBase64(String origin, String charset) {
 		return Base64.encodeBase64String(origin.getBytes(Charset.forName(charset)));
 	}
 
+	/**
+	 * BASE64解密
+	 * @param encrypt  密文
+	 * @param charset  字符编码
+	 * @return
+	 */
 	public static String decodeFromBase64(String encrypt, String charset) {
 		return new String(Base64.decodeBase64(encrypt), Charset.forName(charset));
 	}
