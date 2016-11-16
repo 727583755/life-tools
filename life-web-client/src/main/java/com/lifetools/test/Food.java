03c7fe225bef67632442c328aaf287cd585fa119
zhengke <ke.zheng@quncaotech.com>
2016-11-16 13:57:35 +0800

    完善

---
+++ life-web-client/src/main/java/com/lifetools/test/Food.java
@@ -0,0 +1,11 @@
+package com.lifetools.test;
+
+/**
+ * Created by Zheng.Ke
+ * Date 2016/11/16.
+ */
+public class Food {
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
