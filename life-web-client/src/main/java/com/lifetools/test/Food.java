b80d752bb61fc9ac715665d60b69cb3cba4e30ed
zhengke <ke.zheng@quncaotech.com>
2016-11-16 14:36:18 +0800

    完善

--- life-web-client/src/main/java/com/lifetools/test/Food.java
+++ life-web-client/src/main/java/com/lifetools/test/Food.java
@@ -8,6 +8,8 @@ public class Food {
     public static void main(String[] args) {
         System.out.println("1");
         System.out.println("2");
+        System.out.println("4");
         System.out.println("3");
+        System.out.println("5");
     }
 }
